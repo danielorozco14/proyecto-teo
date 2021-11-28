6b637b34ea01bbec4d1334c16ecabc6e2d3bc5ca
danielorozco14 <00200617@uca.edu.sv>
2021-11-27 23:57:46 -0600

    Terminado

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -11,10 +11,10 @@ reservada = (
     'STD',
     'COUT',
     'CIN',
-   'GET',
-   'CADENA',
-   'RETURN',
-   'VOID',
+    'GET',
+    'CADENA',
+    'RETURN',
+    'VOID',
     'INT',
     'ENDL',
     'STRING'
@@ -30,16 +30,16 @@ tokens = reservada + (
     'POTENCIA',
     'MODULO',
 
-   'MINUSMINUS',
-   'PLUSPLUS',
+    'MINUSMINUS',
+    'PLUSPLUS',
 
-    #Condiones
-     'SI',
+    # Condiones
+    'SI',
     'SINO',
-    #Ciclos
-   'MIENTRAS',
-   'PARA',
-    #logica
+    # Ciclos
+    'MIENTRAS',
+    'PARA',
+    # logica
     'AND',
     'OR',
     'NOT',
@@ -58,13 +58,13 @@ tokens = reservada + (
     'CORDER',
     'LLAIZQ',
     'LLADER',
-    
+
     # Otros
     'PUNTOCOMA',
     'COMA',
     'COMDOB',
-    'MAYORDER', #>>
-    'MAYORIZQ', #<<,
+    'MAYORDER',  # >>
+    'MAYORIZQ',  # <<,
 )
 
 # Reglas de Expresiones Regualres para token de Contexto simple
@@ -100,9 +100,11 @@ def t_INCLUDE(t):
     r'include'
     return t
 
+
 def t_INT(t):
     r'int'
     return t
+
 
 def t_STRING(t):
     r'string'
@@ -113,122 +115,153 @@ def t_USING(t):
     r'using'
     return t
 
+
 def t_NAMESPACE(t):
     r'namespace'
     return t
+
 
 def t_STD(t):
     r'std'
     return t
 
+
 def t_COUT(t):
     r'cout'
     return t
+
 
 def t_CIN(t):
     r'cin'
     return t
 
+
 def t_GET(t):
     r'get'
     return t
+
 
 def t_ENDL(t):
     r'endl'
     return t
 
+
 def t_SINO(t):
     r'else'
     return t
+
 
 def t_SI(t):
     r'if'
     return t
 
+
 def t_RETURN(t):
-   r'return'
-   return t
+    r'return'
+    return t
+
 
 def t_VOID(t):
-   r'void'
-   return t
+    r'void'
+    return t
+
 
 def t_MIENTRAS(t):
     r'while'
     return t
 
+
 def t_PARA(t):
     r'for'
     return t
+
 
 def t_ENTERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+
 def t_IDENTIFICADOR(t):
     r'\w+(_\d\w)*'
     return t
 
+
 def t_CADENA(t):
-   r'\"?(\w+ \ *\w*\d* \ *)\"?'
-   return t
+    r'\"?(\w+ \ *\w*\d* \ *)\"?'
+    return t
+
 
 def t_NUMERAL(t):
     r'\#'
     return t
 
+
 def t_PLUSPLUS(t):
     r'\+\+'
     return t
+
 
 def t_MENORIGUAL(t):
     r'<='
     return t
 
+
 def t_MAYORIGUAL(t):
     r'>='
     return t
+
 
 def t_IGUAL(t):
     r'=='
     return t
 
+
 def t_MAYORDER(t):
     r'<<'
     return t
+
 
 def t_MAYORIZQ(t):
     r'>>'
     return t
 
+
 def t_DISTINTO(t):
     r'!='
     return t
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+
 
 def t_comments(t):
     r'/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
     print("Comentario de multiple linea")
 
-def t_comments_ONELine(t):
-     r'\/\/(.)*\n'
-     t.lexer.lineno += 1
-     print("Comentario de una linea")
-t_ignore =' \t'
 
-def t_error( t):
+def t_comments_ONELine(t):
+    r'\/\/(.)*\n'
+    t.lexer.lineno += 1
+    print("Comentario de una linea")
+
+
+t_ignore = ' \t'
+
+
+def t_error(t):
     global resultado_lexema
     estado = "** Token no valido en la Linea {:4} Valor {:16} Posicion {:4}".format(str(t.lineno), str(t.value),
-                                                                      str(t.lexpos))
+                                                                                    str(t.lexpos))
     resultado_lexema.append(estado)
     t.lexer.skip(1)
 
 # Prueba de ingreso
+
+
 def prueba(data):
     global resultado_lexema
 
@@ -241,9 +274,11 @@ def prueba(data):
         if not tok:
             break
         # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
-        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
+        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(
+            str(tok.lineno), str(tok.type), str(tok.value), str(tok.lexpos))
         resultado_lexema.append(estado)
     return resultado_lexema
+
 
  # instanciamos el analizador lexico
 analizador = lex.lex()
