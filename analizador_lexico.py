58468c22b5d6c4c5c3156363d673ba32d7905d0f
MoisesRosales <45109465+MoisesRosales@users.noreply.github.com>
2021-12-03 23:20:51 -0600

    NADA FURULA

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -12,16 +12,23 @@ reservada = (
     'COUT',
     'CIN',
     'GET',
-    'CADENA',
+    #-----------
+    'CHAR',
+    'INT',
+    'FLOAT',
     'RETURN',
     'VOID',
-    'INT',
     'ENDL',
-    'STRING'
+    #'SI',
+    #'SINO'
 )
-tokens = reservada + (
+tokens = reservada +(
     'IDENTIFICADOR',
     'ENTERO',
+    'FLOTANTE',
+    'NUMERO',
+    'CADENA',
+    'OPERTACION_MATH',
     'ASIGNAR',
     'SUMA',
     'RESTA',
@@ -29,17 +36,21 @@ tokens = reservada + (
     'DIV',
     'POTENCIA',
     'MODULO',
-
     'MINUSMINUS',
     'PLUSPLUS',
 
     # Condiones
     'SI',
     'SINO',
+
     # Ciclos
     'MIENTRAS',
     'PARA',
+    'DO',
+
     # logica
+    'BOOL',
+    'LOGIC',
     'AND',
     'OR',
     'NOT',
@@ -49,9 +60,10 @@ tokens = reservada + (
     'MAYORIGUAL',
     'IGUAL',
     'DISTINTO',
+    'RELATIONAL',
     # Symbolos
-    'NUMERAL',
 
+    'NUMERAL',
     'PARIZQ',
     'PARDER',
     'CORIZQ',
@@ -60,6 +72,8 @@ tokens = reservada + (
     'LLADER',
 
     # Otros
+    'comments_ONELine',
+    'comments',
     'PUNTOCOMA',
     'COMA',
     'COMDOB',
@@ -70,187 +84,206 @@ tokens = reservada + (
 # Reglas de Expresiones Regualres para token de Contexto simple
 
 t_SUMA = r'\+'
-t_RESTA = r'-'
+t_RESTA = r'\-'
 t_MINUSMINUS = r'\-\-'
 # t_PUNTO = r'\.'
 t_MULT = r'\*'
-t_DIV = r'/'
+t_DIV = r'\/'
 t_MODULO = r'\%'
 t_POTENCIA = r'(\*{2} | \^)'
 
-t_ASIGNAR = r'='
+t_ASIGNAR = r'\='
 # Expresiones Logicas
 t_AND = r'\&\&'
 t_OR = r'\|{2}'
 t_NOT = r'\!'
-t_MENORQUE = r'<'
-t_MAYORQUE = r'>'
-t_PUNTOCOMA = ';'
-t_COMA = r','
+t_MENORQUE = r'\<'
+t_MAYORQUE = r'\>'
+t_PUNTOCOMA = '\;'
+t_COMA = r'\,'
 t_PARIZQ = r'\('
 t_PARDER = r'\)'
 t_CORIZQ = r'\['
 t_CORDER = r'\]'
-t_LLAIZQ = r'{'
-t_LLADER = r'}'
+t_LLAIZQ = r'\{'
+t_LLADER = r'\}'
 t_COMDOB = r'\"'
-
-
-def t_INCLUDE(t):
-    r'include'
+#-----------------------------------------------------------------------------------------------------------------------
+#Tipos de Datos
+def t_CHAR(t):
+    r'char'
     return t
-
 
 def t_INT(t):
     r'int'
+    return t 
+
+def t_FLOAT(t):
+    r'float'
+    return t
+#-----------------------------------------------------------------------------------------------------------------------
+#Utilización de variables
+def t_IDENTIFICADOR(t):
+    r'([a-z]|[A-Z]|_*([a-z]|[A-Z]))([a-z]|[A-Z]|\d|_)*'
     return t
 
-
-def t_STRING(t):
-    r'string'
+#-----------------------------------------------------------------------------------------------------------------------
+#Instrucciones: condicional if-else
+def t_SI(t):
+    r'if'
     return t
 
+def t_SINO(t):
+    r'else'
+    return t
+#-----------------------------------------------------------------------------------------------------------------------
+#Declaración y definición de funciones
+def t_VOID(t):
+    r'void'
+    return t
+    
+#-----------------------------------------------------------------------------------------------------------------------
+#Manejo de comentarios 
+def t_comments(t):
+    r'\/\*(.|\n)*\*\/'
+    pass
+
+def t_comments_ONELine(t):
+    r'\/\/.*'
+    pass
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Palabras claves o reservadas: return, using, namespace, std, cout, cin, get, endl
+def t_RETURN(t):
+    r'return'
+    return t
 
 def t_USING(t):
     r'using'
     return t
 
-
 def t_NAMESPACE(t):
     r'namespace'
     return t
-
 
 def t_STD(t):
     r'std'
     return t
 
-
-def t_COUT(t):
-    r'cout'
-    return t
-
-
-def t_CIN(t):
-    r'cin'
-    return t
-
-
 def t_GET(t):
     r'get'
     return t
-
 
 def t_ENDL(t):
     r'endl'
     return t
 
-
-def t_SINO(t):
-    r'else'
+#-----------------------------------------------------------------------------------------------------------------------
+#Cinco operadores aritméticos y cinco operados lógicos 
+#Logicos
+def t_LOGIC(t):
+    r'(\&{2})|(\|{2})'
     return t
-
-
-def t_SI(t):
-    r'if'
-    return t
-
-
-def t_RETURN(t):
-    r'return'
-    return t
-
-
-def t_VOID(t):
-    r'void'
-    return t
-
-
-def t_MIENTRAS(t):
-    r'while'
-    return t
-
-
-def t_PARA(t):
-    r'for'
-    return t
-
-
-def t_ENTERO(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-
-def t_IDENTIFICADOR(t):
-    r'\w+(_\d\w)*'
-    return t
-
-
-def t_CADENA(t):
-    r'\"?(\w+ \ *\w*\d* \ *)\"?'
-    return t
-
-
-def t_NUMERAL(t):
-    r'\#'
-    return t
-
-
-def t_PLUSPLUS(t):
-    r'\+\+'
-    return t
-
 
 def t_MENORIGUAL(t):
     r'<='
     return t
 
-
 def t_MAYORIGUAL(t):
     r'>='
     return t
-
 
 def t_IGUAL(t):
     r'=='
     return t
 
+def t_DISTINTO(t):
+    r'!='
+    return t
 
 def t_MAYORDER(t):
     r'<<'
     return t
 
-
 def t_MAYORIZQ(t):
     r'>>'
     return t
 
-
-def t_DISTINTO(t):
-    r'!='
+def t_RELATIONAL(t):
+    r'(\>=)|(\<=)|(\==)|(\!=)|(\<)|(\>)'
     return t
 
+def t_BOOL(t):
+    r'(true)|(false)'
+    return t
+#-----------------------------------------------------------------------------------------------------------------------
+#Directivas de inclusión y definición de macros 
+def t_INCLUDE(t):
+    r'include'
+    return t
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Instrucciones de iteración: do-while, while o for
+def t_MIENTRAS(t):
+    r'while'
+    return t
+
+def t_PARA(t):
+    r'for'
+    return t
+
+def t_DO(t):
+    r'do'
+    return t
+
+def t_PLUSPLUS(t):
+    r'\+\+'
+    return t
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Funciones para leer desde teclado y escribir en consola
+def t_COUT(t):
+    r'cout'
+    return t
+
+def t_CIN(t):
+    r'cin'
+    return t
+
+#-----------------------------------------------------------------------------------------------------------------------    
+def t_NUMERO(t):
+    r'(\d)+(\.(\d)+)?'
+    if ("." in t.value):
+        t.value = float(t.value)
+    else:
+        t.value = int(t.value)
+    return t
+
+def t_FLOTANTE(t):
+    r'(\d)+(\.(\d)+)?'
+    if ("." in t.value):
+        t.value = float(t.value)
+        return t
+        
+def t_ENTERO(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_CADENA(t):
+    r'(\"?(\w+ \ *\w*\d* \ *)\"?) |(\'?(\w+ \ *\w*\d* \ *)\'?)'
+    return t
+
+def t_NUMERAL(t):
+    r'\#'
+    return t
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-
-def t_comments(t):
-    r'/\*(.|\n)*?\*/'
-    t.lexer.lineno += t.value.count('\n')
-    print("Comentario de multiple linea")
-
-
-def t_comments_ONELine(t):
-    r'\/\/(.)*\n'
-    t.lexer.lineno += 1
-    print("Comentario de una linea")
-
-
 t_ignore = ' \t'
-
 
 def t_error(t):
     global resultado_lexema
@@ -260,7 +293,6 @@ def t_error(t):
     t.lexer.skip(1)
 
 # Prueba de ingreso
-
 
 def prueba(data):
     global resultado_lexema
@@ -278,7 +310,6 @@ def prueba(data):
             str(tok.lineno), str(tok.type), str(tok.value), str(tok.lexpos))
         resultado_lexema.append(estado)
     return resultado_lexema
-
 
  # instanciamos el analizador lexico
 analizador = lex.lex()
--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -7,52 +7,164 @@ resultado_gramatica = []
 
 precedence = (
     ('right', 'ASIGNAR'),
-    ('left', 'SUMA', 'RESTA'),
-    ('left', 'MULT', 'DIV'),
+    ('left', 'SUMA', 'RESTA', 'MULT', 'DIV'),
+    ('left', 'CHAR', 'INT', 'FLOAT'),
     ('right', 'UMINUS'),
     ('right', 'PUNTOCOMA'),
-    ('left', 'SI', 'INT'),
+    ('left', 'SI'),
+    ('right','SINO'),
     ('left', 'PARA'),
-    ('left', 'STRING'),
     ('left','NUMERAL'),
     ('right','INCLUDE'),
-    ('right','SINO')
-
-
+    ('left','PARIZQ' ,'LLAIZQ'),
+    ('right','PARDER', 'LLADER')
 )
 nombres = {}
 
+def p_empty(p):
+    'empty :'
+    pass
 
+#-----------------------------------------------------------------------------------------------------------------------    
+#Declaraciones
 def p_declaracion_asignar(t):
     '''
-        expresion : INT IDENTIFICADOR ASIGNAR expresion PUNTOCOMA
-                    | STRING IDENTIFICADOR ASIGNAR CADENA PUNTOCOMA
+    declaracion_asignar : CHAR IDENTIFICADOR PUNTOCOMA 
+                | INT IDENTIFICADOR PUNTOCOMA
+                | FLOAT IDENTIFICADOR PUNTOCOMA
+
+                | CHAR IDENTIFICADOR CORIZQ  CORDER PUNTOCOMA 
+                | INT IDENTIFICADOR CORIZQ  CORDER PUNTOCOMA 
+                | FLOAT IDENTIFICADOR CORIZQ  CORDER PUNTOCOMA
+
+                | CHAR IDENTIFICADOR CORIZQ NUMERO CORDER PUNTOCOMA 
+                | INT IDENTIFICADOR CORIZQ NUMERO CORDER PUNTOCOMA 
+                | FLOAT IDENTIFICADOR CORIZQ NUMERO CORDER PUNTOCOMA
+
+                | CHAR IDENTIFICADOR COMA declaracion_asignar
+                | INT IDENTIFICADOR COMA declaracion_asignar
+                | FLOAT IDENTIFICADOR COMA declaracion_asignar
+
+                | CHAR IDENTIFICADOR CORIZQ CORDER COMA declaracion_asignar 
+                | INT IDENTIFICADOR CORIZQ  CORDER COMA declaracion_asignar
+                | FLOAT IDENTIFICADOR CORIZQ CORDER COMA declaracion_asignar
+
+                | CHAR IDENTIFICADOR CORIZQ NUMERO CORDER COMA declaracion_asignar 
+                | INT IDENTIFICADOR CORIZQ  NUMERO CORDER COMA declaracion_asignar
+                | FLOAT IDENTIFICADOR CORIZQ NUMERO CORDER COMA declaracion_asignar
+
+                | IDENTIFICADOR COMA declaracion_asignar
+                | IDENTIFICADOR PUNTOCOMA
+
+                | IDENTIFICADOR CORIZQ CORDER COMA declaracion_asignar
+                | IDENTIFICADOR CORIZQ CORDER PUNTOCOMA
+
+                | IDENTIFICADOR CORIZQ NUMERO CORDER COMA declaracion_asignar
+                | IDENTIFICADOR CORIZQ NUMERO CORDER PUNTOCOMA
+
+                | INT IDENTIFICADOR ASIGNAR NUMERO PUNTOCOMA
+                | CHAR IDENTIFICADOR ASIGNAR CADENA PUNTOCOMA
+                | FLOAT IDENTIFICADOR ASIGNAR NUMERO PUNTOCOMA
+
+                | IDENTIFICADOR ASIGNAR NUMERO PUNTOCOMA
+                | IDENTIFICADOR ASIGNAR CADENA PUNTOCOMA
+                
+                | INT IDENTIFICADOR ASIGNAR IDENTIFICADOR PUNTOCOMA
+                | CHAR IDENTIFICADOR ASIGNAR IDENTIFICADOR PUNTOCOMA
+                | FLOAT IDENTIFICADOR ASIGNAR IDENTIFICADOR PUNTOCOMA
+                
     '''
-    #nombres[t[1]] = t[3]
-    nombres[t[2]] = t[4]
-
-
-def p_declaracion_expr(t):
-    'declaracion : expresion'
-    # print("Resultado: " + str(t[1]))
-    t[0] = t[1]
-
-
-def p_expresion_if(t):
+#-----------------------------------------------------------------------------------------------------------------------     
+#Condicional  if-else
+def p_expresion_SI_SINO(t):
     '''
-    expresion : SI PARIZQ ENTERO MAYORQUE ENTERO PARDER LLAIZQ LLADER SINO LLAIZQ LLADER
-                | SI PARIZQ ENTERO MENORQUE ENTERO PARDER LLAIZQ LLADER SINO LLAIZQ LLADER
-                | SI PARIZQ ENTERO IGUAL ENTERO PARDER LLAIZQ LLADER SINO LLAIZQ LLADER
+    if : SI PARIZQ condiciones_logicas PARDER
+        | SI PARIZQ condiciones_logicas PARDER LLAIZQ instruccion LLADER
+        | SI PARIZQ condiciones_logicas PARDER LLAIZQ instruccion LLADER SINO PARDER LLAIZQ instruccion LLADER
     '''
-    # if(3<5)
+#-----------------------------------------------------------------------------------------------------------------------  
+#Manejo de comentarios
+def p_comentario_lienal(t):
+    '''
+    comentario_lineal : comments_ONELine  
+    '''
 
+def p_comentario_bloque(t):
+    '''
+    comentario_bloque : comments  
+    '''
+#-----------------------------------------------------------------------------------------------------------------------    
+#Operaciones Logicas y Matematicas
+def p_expresion_operaciones_math(t):
+    '''
+    operaciones_math : NUMERO SUMA NUMERO PUNTOCOMA
+                |   NUMERO RESTA NUMERO PUNTOCOMA
+                |   NUMERO MULT NUMERO PUNTOCOMA
+                |   NUMERO DIV NUMERO PUNTOCOMA
+                |   NUMERO POTENCIA NUMERO PUNTOCOMA
+                |   NUMERO MODULO NUMERO PUNTOCOMA 
 
+                |   NUMERO SUMA NUMERO operaciones_math
+                |   NUMERO RESTA NUMERO operaciones_math
+                |   NUMERO MULT NUMERO operaciones_math
+                |   NUMERO DIV NUMERO operaciones_math
+                |   NUMERO POTENCIA NUMERO operaciones_math
+                |   NUMERO MODULO NUMERO operaciones_math
+
+                |   SUMA NUMERO PUNTOCOMA
+                |   RESTA NUMERO PUNTOCOMA
+                |   MULT NUMERO PUNTOCOMA
+                |   DIV NUMERO PUNTOCOMA
+                |   POTENCIA NUMERO PUNTOCOMA
+                |   MODULO NUMERO PUNTOCOMA
+           
+    '''
+def p_operacion_booleana(t):
+    '''
+    operacion_booleana : expresion AND expresion 
+                |  expresion OR expresion 
+                |  expresion NOT expresion 
+    '''
+def p_condiciones_logicas(p):
+    '''
+    condiciones_logicas : IDENTIFICADOR RELATIONAL IDENTIFICADOR
+            | NUMERO RELATIONAL NUMERO
+            | IDENTIFICADOR RELATIONAL IDENTIFICADOR condiciones_logicas
+            | IDENTIFICADOR LOGIC condiciones_logicas
+            | BOOL 
+            | BOOL LOGIC condiciones_logicas
+
+    '''
+#-----------------------------------------------------------------------------------------------------------------------  
+#Ciclos
+def p_expresion_mientras(t):
+    '''
+      while : MIENTRAS PARIZQ condiciones_logicas PARDER LLAIZQ instruccion LLADER 
+    '''
+def p_expresion_do(t):
+    '''
+      do : DO PARIZQ instruccion PARDER while 
+    '''
+#-----------------------------------------------------------------------------------------------------------------------  
+
+def p_instruccion(t):
+    '''
+    instruccion : declaracion_asignar
+                | if
+                | comentario_lineal
+                | comentario_bloque
+                | operacion_booleana
+                | condiciones_logicas
+                | while
+                | do
+                | empty
+
+    '''
 def p_expresion_array(t):
     '''
     expresion  :   INT IDENTIFICADOR CORIZQ ENTERO CORDER ASIGNAR LLAIZQ ENTERO COMA ENTERO COMA ENTERO COMA ENTERO LLADER PUNTOCOMA
 
     '''
-
 
 def p_expresion_include(t):
     '''
@@ -60,132 +172,14 @@ def p_expresion_include(t):
 
     '''
 
-def p_expresion_mientras(t):
-    '''
-      expresion : MIENTRAS PARIZQ ENTERO MENORQUE ENTERO PARDER LLAIZQ expresion LLADER
-    '''
-
-
-def p_expresion_operaciones(t):
-    '''
-    expresion  :   expresion SUMA expresion
-                |   expresion RESTA expresion
-                |   expresion MULT expresion
-                |   expresion DIV expresion
-                |   expresion POTENCIA expresion
-                |   expresion MODULO expresion
-
-    '''
-    if t[2] == '+':
-        t[0] = t[1] + t[3]
-    elif t[2] == '-':
-        t[0] = t[1] - t[3]
-    elif t[2] == '*':
-        t[0] = t[1] * t[3]
-    elif t[2] == '/':
-        t[0] = t[1] / t[3]
-    elif t[2] == '%':
-        t[0] = t[1] % t[3]
-    elif t[2] == '**':
-        i = t[3]
-        t[0] = t[1]
-        while i > 1:
-            t[0] *= t[1]
-            i -= 1
-
-
 def p_expresion_uminus(t):
     'expresion : RESTA expresion %prec UMINUS'
     t[0] = -t[2]
 
 
-def p_expresion_grupo(t):
-    '''
-    expresion  : PARIZQ expresion PARDER
-                | LLAIZQ expresion LLADER
-                | CORIZQ expresion CORDER
-    '''
-    t[0] = t[2]
-# sintactico de expresiones logicas
-
-
-def p_expresion_logicas(t):
-    '''
-    expresion   :  expresion MENORQUE expresion 
-                |  expresion MAYORQUE expresion 
-                |  expresion MENORIGUAL expresion 
-                |   expresion MAYORIGUAL expresion 
-                |   expresion IGUAL expresion 
-                |   expresion DISTINTO expresion
-                |  PARIZQ expresion PARDER MENORQUE PARIZQ expresion PARDER
-                |  PARIZQ expresion PARDER MAYORQUE PARIZQ expresion PARDER
-                |  PARIZQ expresion PARDER MENORIGUAL PARIZQ expresion PARDER 
-                |  PARIZQ  expresion PARDER MAYORIGUAL PARIZQ expresion PARDER
-                |  PARIZQ  expresion PARDER IGUAL PARIZQ expresion PARDER
-                |  PARIZQ  expresion PARDER DISTINTO PARIZQ expresion PARDER
-    '''
-    if t[2] == "<":
-        t[0] = t[1] < t[3]
-    elif t[2] == ">":
-        t[0] = t[1] > t[3]
-    elif t[2] == "<=":
-        t[0] = t[1] <= t[3]
-    elif t[2] == ">=":
-        t[0] = t[1] >= t[3]
-    elif t[2] == "==":
-        t[0] = t[1] is t[3]
-    elif t[2] == "!=":
-        t[0] = t[1] != t[3]
-    elif t[3] == "<":
-        t[0] = t[2] < t[4]
-    elif t[2] == ">":
-        t[0] = t[2] > t[4]
-    elif t[3] == "<=":
-        t[0] = t[2] <= t[4]
-    elif t[3] == ">=":
-        t[0] = t[2] >= t[4]
-    elif t[3] == "==":
-        t[0] = t[2] is t[4]
-    elif t[3] == "!=":
-        t[0] = t[2] != t[4]
-
-    # print('logica ',[x for x in t])
-
-# gramatica de expresiones booleanadas
-
-
-def p_expresion_booleana(t):
-    '''
-    expresion   :   expresion AND expresion 
-                |   expresion OR expresion 
-                |   expresion NOT expresion 
-                |  PARIZQ expresion AND expresion PARDER
-                |  PARIZQ expresion OR expresion PARDER
-                |  PARIZQ expresion NOT expresion PARDER
-    '''
-    if t[2] == "&&":
-        t[0] = t[1] and t[3]
-    elif t[2] == "||":
-        t[0] = t[1] or t[3]
-    elif t[2] == "!":
-        t[0] = t[1] is not t[3]
-    elif t[3] == "&&":
-        t[0] = t[2] and t[4]
-    elif t[3] == "||":
-        t[0] = t[2] or t[4]
-    elif t[3] == "!":
-        t[0] = t[2] is not t[4]
-
-
-def p_expresion_numero(t):
-    'expresion : ENTERO'
-    t[0] = t[1]
-
-
 def p_expresion_cadena(t):
     'expresion : COMDOB expresion COMDOB'
     t[0] = t[2]
-
 
 def p_expresion_nombre(t):
     'expresion : IDENTIFICADOR'
@@ -194,7 +188,6 @@ def p_expresion_nombre(t):
     except LookupError:
         print("Nombre desconocido ", t[1])
         t[0] = 0
-
 
 def p_error(t):
     global resultado_gramatica
